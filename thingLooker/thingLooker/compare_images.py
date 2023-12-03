37a7f6273695a40e4b105da8649d36b46d4404bb
Luke Witten <lhwitten@gmail.com>
2023-12-02 19:46:39 -0500

    some framework for ros pipeline

--- thingLooker/thingLooker/compare_images.py
+++ thingLooker/thingLooker/compare_images.py
@@ -11,6 +11,7 @@ from sentence_transformers import SentenceTransformer, util
 from PIL import Image
 import glob
 import os
+import matplotlib.pyplot as plt
 
 def image_compare_SSIM(img1,img2):
 
@@ -142,12 +143,15 @@ def get_camera():
         if key == ord('b'):
             compare_and_visualize_differences(frame1,frame2)
 
-    # After the loop release the cap object 
+    # After the loop release the cap objectq
     vid.release() 
     # Destroy all the windows 
-    cv2.destroyAllWindows() 
+    cv2.destroyAllWindows()
 
 def compare_and_visualize_differences(img1, img2, min_contour_area=100):
+    
+    #img1,img2 = replace_blurry_regions(img1,img2,blur_threshold=65)
+    
     # Convert images to grayscale
     first_gray = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
     second_gray = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
@@ -189,7 +193,52 @@ def compare_and_visualize_differences(img1, img2, min_contour_area=100):
     cv2.waitKey(0)
     #cv2.destroyAllWindows()
 
+#blur functions
 
+def detect_blurry_regions(image, threshold=100):
+    """
+    Detect blurry regions in an image using the Laplacian method.
+    Returns a mask indicating blurry regions.
+    """
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    laplacian = cv2.Laplacian(gray, cv2.CV_64F)
+    laplacian_var = laplacian.var()
+
+    # If variance is less than the threshold, it's considered blurry
+    mask = np.where(laplacian_var < threshold, 0, 1).astype('uint8')
+    
+    
+    visualize = True
+    if visualize:
+        plt.figure(figsize=(10, 4))
+        plt.subplot(1, 2, 1)
+        plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+        plt.title('Original Image')
+        plt.axis('off')
+        plt.subplot(1, 2, 2)
+        plt.imshow(laplacian, cmap='gray')
+        plt.title('Laplacian Gradient')
+        plt.axis('off')
+        plt.show()
+    
+    return mask
+
+def replace_blurry_regions(img1, img2, blur_threshold=100):
+    """
+    Detects blurry regions in both images and replaces those regions with black pixels in both images.
+    """
+    # Detect blurry regions in both images
+    mask1 = detect_blurry_regions(img1, blur_threshold)
+    mask2 = detect_blurry_regions(img2, blur_threshold)
+
+    # Combine masks to identify all blurry regions in both images
+    combined_mask = np.bitwise_or(mask1, mask2)
+
+    # Apply combined mask to both images
+    img1[combined_mask == 0] = [0, 0, 0]
+    img2[combined_mask == 0] = [0, 0, 0]
+
+    return img1, img2
 
 
 get_camera()
---
+++ thingLooker/thingLooker/explore.py
@@ -0,0 +1,93 @@
+import rclpy
+from rclpy.node import Node
+from geometry_msgs.msg import Twist
+from geometry_msgs.msg import PoseWithCovariance
+from geometry_msgs.msg import Quaternion
+from nav_msgs.msg import Odometry
+import math
+import numpy as np
+
+
+from sensor_msgs.msg import Image
+from std_srvs.srv import Trigger
+
+##robot knows where it is
+class position_knower(Node):
+    def __init__(self):
+            super().__init__('position_knower')
+            
+            self.create_subscription(Odometry, 'odom', self.process_odom, 10)
+
+            self.create_subscription(Image, 'camera/image_raw', self.get_cam, 10)
+            self.last_cam = None
+
+            #camera/image_raw
+            self.vel_pub = self.create_publisher(Twist,'cmd_vel',10)
+            self.create_timer(.05,self.run_loop)
+
+            #robot position
+            self.xpos = 0.0
+            self.ypos = 0.0
+            self.zpos = 0.0
+
+            self.cam_phi = np.pi/16
+            #self.angular = 0.0
+
+            #position of last turn
+            self.xpos_b = 0.0
+            self.ypos_b = 0.0
+            self.orientation_bench = Quaternion()
+
+            self.orientation = Quaternion()
+            self.wait = False
+            self.w_count = 0
+
+
+
+    def run_loop(self):
+        pass
+
+    def get_cam(self,msg):
+        
+        self.last_cam = msg
+    
+    def process_odom(self,msg):
+
+        #PoseWithCovariance.pose.
+        self.xpos = msg.pose.pose.position.x
+        self.ypos = msg.pose.pose.position.y
+
+        #robot_q = msg.pose.pose.orientation
+        #w,x,y,z
+        #my_quaternion = pyq.Quaternion(robot_q.w,robot_q.x,robot_q.y,robot_q.z)
+        #self.orientation = my_quaternion
+        self.orientation = msg.pose.pose.orientation
+
+
+    
+    def image_compare(self):
+         
+         #camera_img = camera()
+
+         NeRF_img = Nerf.forward(self.xpos,self.ypos,self.zpos,THETA,self.cam_phi)
+
+         camera_img, NeRF_img = resize_images(self.last_cam, NeRF_img)
+
+
+         compare_images()
+
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = position_knower()
+    rclpy.spin(node)
+    rclpy.shutdown()
+
+def quat_angle(q1,q2):
+    #angle in degrees between two quaternion objects
+    inner_prod =  q1.w *q2.w + q1.x *q2.x + q1.y + q2.y + q1.z * q2.z
+
+    return 180/np.pi * np.arccos(2 * (inner_prod**2) -1)
+
+###
